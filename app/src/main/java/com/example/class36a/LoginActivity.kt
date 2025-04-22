1cfaac3a7f449c5d80fa056352aee3d7388406f3
SarunMr <sarunmaharjan38@gmail.com>
2025-04-22 09:40:48 +0545

    Done an Icon

--- app/src/main/java/com/example/class36a/LoginActivity.kt
+++ app/src/main/java/com/example/class36a/LoginActivity.kt
@@ -1,6 +1,6 @@
 package com.example.class36a
 
-import android.content.res.Configuration
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,38 +8,50 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import java.nio.file.WatchEvent
+import androidx.compose.ui.unit.dp
 
-class LoginActivity  : ComponentActivity() {
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            Scaffold {innerPadding ->
-                LoginBody(innerPadding)
+            Scaffold { innerPadding ->
+                InstaClone(innerPadding)
             }
         }
     }
 }
 
 @Composable
-fun LoginBody(innerPadding : PaddingValues) {
-    Column (
+fun InstaClone(innerPadding : PaddingValues){
+    Column(
         modifier =
             Modifier
-            .padding(innerPadding)
-            .fillMaxSize()
-            .background(color = Color.Green)
-    ){
+                .padding(innerPadding)
+                .fillMaxSize()
+                .background(color = Color.White)
+    ) {
+        Row() {
+            Icon(imageVector = Icons.Default.KeyboardArrowLeft, contentDescription = null)
 
+        }
     }
-
 }
 @Preview(showBackground = true)
+@Composable
+fun PreviewInsta() {
+    InstaClone(innerPadding = PaddingValues(0.dp))
+
+}
