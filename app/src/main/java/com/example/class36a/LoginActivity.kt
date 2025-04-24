c7a5e16f574dd0aab99ba4a06a95bcb94223de99
SarunMr <sarunmaharjan38@gmail.com>
2025-04-24 18:41:30 +0545

    Insta_Added-Buttons

--- app/src/main/java/com/example/class36a/LoginActivity.kt
+++ app/src/main/java/com/example/class36a/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -20,14 +21,19 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -78,7 +84,11 @@ fun InstaClone(innerPadding : PaddingValues){
                 modifier = Modifier.height(50.dp).width(50.dp)
             )
         }
-        Row (){
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp, vertical = 20.dp)
+        ){
             Image(
                 painter = painterResource(R.drawable.person),
                 contentDescription = null,
@@ -86,6 +96,95 @@ fun InstaClone(innerPadding : PaddingValues){
                     .height(120.dp).width(120.dp)
                     .clip(shape = RoundedCornerShape(100.dp))
             )
+            Column (
+               verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text("174")
+                Text("Posts")
+            }
+
+            Column (
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text("177k")
+                Text("Followers")
+            }
+
+            Column (
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text("774")
+                Text("Following")
+            }
+        }
+        Column(
+            modifier = Modifier.padding(
+                horizontal = 15.dp,
+                vertical = 15.dp
+            )
+        ){
+            Text("Andrew Queo")
+            Text("Programmer", modifier = Modifier.alpha(alpha = 0.5f))
+            Text("I use neovim btw")
+            Text("issbella@art.com")
+            Text("Followed by jeena and anna")
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp, vertical = 10.dp)
+        ) {
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue,
+                    contentColor = Color.White
+                ),
+                ) {
+                Text(text = "Follow")
+            }
+
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(1.dp,Color.Gray),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.Black
+                ),
+            ) {
+                Text(text = "Message")
+            }
+
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(1.dp,Color.Gray),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.Black
+                ),
+            ) {
+                Text(text = "Email")
+            }
+
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(1.dp,Color.Gray),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.Black
+                ),
+            ) {
+                Icon(
+                    painterResource(R.drawable.baseline_keyboard_arrow_down_24),
+                    contentDescription = null,
+                    modifier = Modifier.height(25.dp).width(25.dp)
+                )
+            }
+
         }
     }
 }
